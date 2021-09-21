3828cd32a3de52dcdad994cf031e01a470be20c4
AzizaChipep <90821959+AzizaChipep@users.noreply.github.com>
2021-09-21 15:42:15 +0500

    Добавьте файлы проекта.

---
+++ Matrixx/Matrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Matrixx
+{
+    class Matrix
+    {
+        static object locker = new object();
+
+        static string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        static char GetLetters()
+        {
+            return letters.ToCharArray()[new Random().Next(letters.Length)];
+        }
+
+        static public void ThreadMethod(string indent, int delay)
+        {
+            for (int i = 0; i < letters.Length ; i++ )
+            {
+                lock (locker)
+                {
+                    
+                    Console.WriteLine(indent + GetLetters());
+                    Thread.Sleep(delay);
+
+                    if (i%2==0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    
+
+                    
+                }
+
+            }
+        }
+    }
+}
---
+++ Matrixx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Matrixx
+{
+    class Program:Matrix
+    {
+        static void Main(string[] args)
+        {
+           
+            Random random = new Random();
+            while (true)
+            {
+                var Thread = new Thread(
+                  () => ThreadMethod(new string(' ', random.Next(5, 60)), random.Next(10, 100)));
+                Thread.Start();
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
